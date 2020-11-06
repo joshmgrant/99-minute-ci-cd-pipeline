794bf9c66a0efdc17000ff6d82d9c1ba0bcca103
joshgrant <joshua.m.grant@gmail.com>
2020-11-06 10:31:53 -0500

    test update

--- test/specs/functional.spec.js
+++ test/specs/functional.spec.js
@@ -5,7 +5,7 @@ describe('Temperature converter', () => {
         browser.url('/');
         
         const actualTitle = $(".App-title").getText();
-        const expectedTitle = "Welcome to Celcius to Farhenheit Calculator!"
+        const expectedTitle = "Welcome to Celsius to Fahrenheit Calculator!"
 
         assert.strictEqual(actualTitle, expectedTitle);
     });
